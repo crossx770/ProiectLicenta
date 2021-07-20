d315d263f79623e9abdfbc14407d01d9beec0a9f
crossx770 <bobolemnaru98@gmail.com>
2021-07-20 09:53:22 +0300

    removed old project

--- LicentaFinal/src/main/java/com/licenta/domain/User.java
+++ LicentaFinal/src/main/java/com/licenta/domain/User.java
@@ -82,6 +82,7 @@ public class User extends AbstractAuditingEntity implements Serializable {
     @Column(name = "reset_date")
     private Instant resetDate = null;
 
+    @Column(name = "")
     @JsonIgnore
     @ManyToMany
     @JoinTable(
