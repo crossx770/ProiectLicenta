c99e26974483643b362c30230ee92baecf9128c9
crossx770 <bobolemnaru98@gmail.com>
2021-07-27 22:18:26 +0300

    Initial version of Licenta generated by generator-jhipster@7.1.0

---
+++ LicentaFinal2/src/main/java/licenta/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package licenta.repository;
+
+import licenta.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
